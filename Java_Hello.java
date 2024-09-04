42d24bc186cc4c4a98fef2ae69e9dc55bc92664a
unknown <Ka-4517>
2024-09-04 18:21:40 +0530

    Initial commit

---
+++ Java_Hello.java
@@ -0,0 +1,12 @@
+package Java_P01_Defualt;
+
+public class Java_Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!");
+		
+
+	}
+
+}
---
+++ Webdriver_setting.java
@@ -0,0 +1,22 @@
+package Java_P01_Defualt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Webdriver_setting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://google.com/");
+		String Title=driver.getTitle();
+		System.out.println(Title);
+		System.out.println(driver.getCurrentUrl());
+		driver.close();
+		driver.quit();
+		
+
+		
+	}
+
+}
